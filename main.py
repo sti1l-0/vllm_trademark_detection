4b650a9575474819c93a218a5a2679b54fad87be
sti1l-0 <1737499156@qq.com>
2024-12-19 22:31:09 +0800

    小小debug

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ if __name__ == "__main__":
 
     bot = vllm.bot("./config.yml")
     pool = vllm.ThreadPool("./config.yml")
-    for _ in range(2):
-        pool.add_task(func=bot.trademark_detect, args=("./image.jpg",))
+    for image in images:
+        pool.add_task(func=bot.trademark_detect, args=(f"{frame_folder}/{image}",))
     pool.close()
     results = pool.get_results()
 
-    conclusion = vllm.conclude(results)
+    conclusion = bot.conclude(results)
     print(conclusion)
